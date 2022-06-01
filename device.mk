43a4995f071b99573e5b8ae9e654c52d3046622f
miramax001 <106231529+miramax001@users.noreply.github.com>
2022-06-01 13:58:55 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -24,6 +24,8 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
+TARGET_BOARD_PLATFORM  := sdm845
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
